2efc170fb1b2bcc7f5f62be6466c024963deac09
adamnilsson <adam.nilss@gmail.com>
2012-12-13 09:44:22 -0800

    experimental estimators

--- src/boot_agents/diffeo/learning/__init__.py
+++ src/boot_agents/diffeo/learning/__init__.py
@@ -1,2 +1,7 @@
 from .. import np, contract, logger
 from .diffeo_estimator_fast import *
+from .diffeo_estimator_fast_probability import *
+from .diffeo_estimator_animation import *
+from .diffeo_estimator_fourier import *
+from .diffeo_estimator_pixelized import *
+from .image_interpolator import *
