3ff20ca58b9b972f07e702d2e2dbb95b4f3d403a
adamnilsson <adam.nilss@gmail.com>
2013-03-12 13:14:29 -0700

    optional min and max values for diffeo visualization function in case some functions want to use that

--- src/boot_agents/diffeo/diffeo_visualization.py
+++ src/boot_agents/diffeo/diffeo_visualization.py
@@ -1,13 +1,19 @@
 from reprep.graphics.filter_scale import scale
 import numpy as np
 
-def scalaruncertainty2rgb(x):
-    """ Converts the scalar uncertainty (in [0,1]) to rgb. (green=1, red=0) """
+def scalaruncertainty2rgb(x, min=0, max=1):
+    """ Converts the scalar uncertainty (in [min, max]) to rgb. (green=1, red=0) """
     # set exactly 0 to nan, and color it gray
     x = x.copy()
+    
+#    if min is None:
+#        min = np.min(x)
+#    if max is None:
+#        max = np.nanmax(x)
+    
     x[x == 0] = np.nan
     
-    rgb = scale(x, max_value=1, min_value=0,
+    rgb = scale(x, max_value=max, min_value=min,
                    min_color=[1, 0, 0], max_color=[0, 1, 0],
                    nan_color=[0.5, 0.5, 0.5])
     return rgb
